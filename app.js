15a98fb0e7906d0bd88c4f19c2013058bf8e6c5f
Aileen Ocampo <aileenocampo24@gmail.com>
2021-06-26 01:03:18 +1000

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const port = 5000;
+var port = 5000;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { requiresArg } = require('yargs');
 var router = express.Router();
 
 // IN-MEMORY STORAGE FOR MOVIES
@@ -30,16 +31,48 @@ const movies = [
 //GET MOVIES
 router.get("/", (req, res) => {
   res.status(200).send(movies);
-})
+});
 
-router.get("/:id", (req, res) => {
-  const { movieId } = req.params;
-  console.log(req.params);
-  if (!movies[movieId]) {
-    res.status(404).send(`Student ${movieId} not found!`)
+//GET MOVIES
+router.get("/", (req, res) => {
+  const movieList = Object.values(title)
+  if(req.query.search) {
+    const filteredMoviesList = movies.filter(movie => movie.title.includes(req.query.search));
+    res.status(200).send(filteredMoviesList)
   } else {
-    res.status(200).send(movies)
+    res.status(200).send("All movies: " + movieList)
   }
+});
+
+//GET MOVIES BY ID
+router.get("/:id", (req, res) => {
+  const movie = movies.find(movie => movie.id === parseInt(req.params.id));
+  if (!movie) res.status(404).send(`The movie with id: ${req.params.id} does not exist`)
+  res.status(200).send(movie);
+});
+
+//ADD A NEW MOVIE
+router.post("/", (req, res) => {
+  const movie = {
+    id: movies.length + 1,
+    title: req.body.title,
+    runtime: req.body.runtime,
+    release_year: req.body.release_year,
+    director: req.body.director,
+  };
+  movies.push(movie);
+  res.status(201).send(`Movie saved: ${movie}`);
+});
+
+// router.delete()
+router.delete("/:id", (req, res) => {
+  const movie = movies.find(movie => movie.id === parseInt(req.params.id));
+  if (!movie) res.status(404).send(`The movie with id: ${req.params.id} does not exist`)
+
+  const index = movies.indexOf(movie);
+  movies.splice(index, 1);
+
+  res.send(`Movie deleted: ${movie.title}`)
 })
 
 module.exports = router;
